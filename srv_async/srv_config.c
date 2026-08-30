f5546653a8807d107a790dfab6d96f94ea7eb4f4
Andrey Alekhin <andrei.aleohin@gmail.com>
2026-08-30 18:44:36 +0300

    Async server: unify parsing of config option for config file and cmdline options
    Signed-off-by: Andrey Alekhin <andrei.aleohin@gmail.com>

--- srv_async/srv_config.c
+++ srv_async/srv_config.c
@@ -12,87 +12,104 @@
 srv_config_t SRV_CONFIG;
 
 const char HELP_MSG[] = "Supported cmdline options for server:\n"
-                  "-w NUM_WORKERS - number of worker processes "
+                  "-w ('num_workers') NUM_WORKERS - number of worker processes "
                   "to create, should be from 1 to 1000\n"
-                  "-p PORT_NUM - port number to listen on server, "
+                  "-p ('port') PORT_NUM - port number to listen on server, "
                   "should be from 1 to 65536 (max valid port)\n"
-                  "-f path_to_log_file - file for reporting errors "
+                  "-f ('log_file') path_to_log_file - file for reporting errors "
                   "and info\n"
-                  "-l log_level - verbosity of info prints in log\n"
+                  "-l ('log_level') log_level - verbosity of info prints in log\n"
                   "-h - get this help\n";
+
+static int parse_range_value(const char *val_str, int *val, int min, int max,
+                            const char *p_name, bool is_config)
+{
+    int tmp;
+
+    tmp = atoi(val_str);
+    if (tmp < min || tmp > max) {
+        fprintf(stderr, "%s(): invalid value %d for '%s' "
+                "in %s, should be in range [%d-%d]\n",
+                __func__, tmp, p_name,
+                is_config ? SRV_CONFIG_FILE : "cmdline",
+                min, max);
+        return -ERANGE;
+    }
+
+    *val = tmp;
+
+    return 0;
+}
+
+static int parse_log_file_name(const char *val_str, bool is_config)
+{
+    SRV_CONFIG.log_file_name = malloc(strlen(val_str) + 1);
+    if (!SRV_CONFIG.log_file_name) {
+        fprintf(stderr, "%s(): could not allocate %ld bytes for "
+                "log_file name %s in %s\n", __func__,
+                strlen(val_str) + 1, val_str,
+                is_config ? SRV_CONFIG_FILE : "cmdline");
+        return -ENOMEM;
+    }
+
+    strcpy(SRV_CONFIG.log_file_name, val_str);
+    SRV_CONFIG.log_file_desc = fopen(val_str, "w");
+
+    if (!SRV_CONFIG.log_file_desc) {
+        fprintf(stderr, "%s(): could not open 'log_file'=%s "
+                "from %s: %s\n", __func__, val_str,
+                is_config ? SRV_CONFIG_FILE : "cmdline",
+                strerror(errno));
+        return -errno;
+    }
+
+    return 0;
+}
 
 int parse_srv_cmdline_opts(int argc, char *argv[])
 {
-    int c, ret = 0;
-    long val;
-    char *endptr;
+    int c, val, ret;
 
-    while ((c = getopt(argc, argv, "w:p:h")) != -1) {
+    while ((c = getopt(argc, argv, "w:p:f:l:h")) != -1) {
         switch (c) {
-        case 'w': {
-            endptr = NULL;
-            val = strtol(optarg, &endptr, 10);
-            if (*endptr != '\0' || errno == ERANGE ||
-                        val < 1 || val > MAX_NUM_WORKERS) {
-                fprintf(stderr, "Srv: invalid value %ld for -w "
-                                "option\n", val);
-                ret = -EINVAL;
-                break;
-            }
-            SRV_CONFIG.num_workers = (int) val;
-            break;
-        }
-        case 'p': {
-            endptr = NULL;
-            val = strtol(optarg, &endptr, 10);
-            if (*endptr != '\0' || errno == ERANGE ||
-                        val < 1 || val > 65535) {
-                fprintf(stderr, "Srv: invalid value %ld for -p "
-                                "option\n", val);
-                ret = -EINVAL;
-                break;
-            }
-            SRV_CONFIG.port = (int) val;
-            break;
-        }
-        case 'f': {
-            SRV_CONFIG.log_file_name = malloc(strlen(optarg) + 1);
-            if (!SRV_CONFIG.log_file_name) {
-                fprintf(stderr, "%s(): could not allocate %ld bytes for "
-                        "log_file name\n", __func__, strlen(optarg) + 1);
-                return -ENOMEM;
-            }
+        case 'w':
+            ret = parse_range_value(optarg, &val, 1, MAX_NUM_WORKERS,
+                                    "num_workers", false);
+            if (ret)
+                return ret;
 
-            strcpy(SRV_CONFIG.log_file_name, optarg);
-            SRV_CONFIG.log_file_desc = fopen(optarg, "w");
+            SRV_CONFIG.num_workers = val;
+            break;
+        case 'p':
+            ret = parse_range_value(optarg, &val, 1, 65535, "port", false);
+            if (ret)
+                return ret;
 
-            if (!SRV_CONFIG.log_file_desc) {
-                fprintf(stderr, "%s(): could not open 'log_file'=%s "
-                        "from %s: %s\n", __func__, optarg,
-                        SRV_CONFIG_FILE, strerror(errno));
-                return -errno;
-            }
-        }
-        case 'l': {
-            val = atoi(optarg);
-            if (val < LOG_ERROR || val > LOG_INFO2) {
-                fprintf(stderr, "%s(): invalid value %ld for 'log_level' "
-                        "in %s, should be in range [%d-%d]\n",
-                        __func__, val, SRV_CONFIG_FILE, LOG_ERROR,
-                        LOG_INFO2);
-                return -ERANGE;
-            }
-        }
+            SRV_CONFIG.port = val;
+            break;
+        case 'f':
+            ret = parse_log_file_name(optarg, false);
+            if (ret)
+                return ret;
+            break;
+        case 'l':
+            ret = parse_range_value(optarg, &val, LOG_ERROR, LOG_INFO2,
+                                    "log_level", false);
+            if (ret)
+                return ret;
+
+            SRV_CONFIG.log_level = val;
+            break;
         case 'h':
             SRV_CONFIG.help = true;
             break;
         default:
             SRV_CONFIG.help = true;
-            ret = -EINVAL;
+            return -EINVAL;
         }
     }
 
-    return ret;
+    return 0;
 }
 
 static char *trim_spaces(char *s)
@@ -117,7 +134,7 @@ static char *trim_spaces(char *s)
 static int parse_srv_config_line(char *line)
 {
     char *key, *val_str;
-    int val;
+    int val, ret;
 
     key = trim_spaces(line);
 
@@ -135,47 +152,29 @@ static int parse_srv_config_line(char *line)
         return 0;
 
     if (strcmp(key, "port") == 0) {
-        val = atoi(val_str);
-        if (val == 0 || val < 1 || val > 65535) {
-            fprintf(stderr, "Srv: invalid value %d for 'port' "
-                            "in config file\n", val);
-            return -EINVAL;
-        }
+        ret = parse_range_value(val_str, &val, 1, 65535,
+                                "port", true);
+        if (ret)
+            return ret;
+
         SRV_CONFIG.port = val;
     } else if (strcmp(key, "num_workers") == 0) {
-        val = atoi(val_str);
-        if (val == 0 || val < 1 || val > MAX_NUM_WORKERS) {
-            fprintf(stderr, "Srv: invalid value %d for 'num_workers' "
-                            "in config file\n", val);
-            return -EINVAL;
-        }
+        ret = parse_range_value(val_str, &val, 1, MAX_NUM_WORKERS,
+                                "num_workers", true);
+        if (ret)
+            return ret;
+
         SRV_CONFIG.num_workers = val;
     } else if (strcmp(key, "log_file") == 0) {
-        SRV_CONFIG.log_file_name = malloc(strlen(val_str) + 1);
-        if (!SRV_CONFIG.log_file_name) {
-            fprintf(stderr, "%s(): could not allocate %ld bytes for "
-                    "log_file name\n", __func__, strlen(val_str) + 1);
-            return -ENOMEM;
-        }
-
-        strcpy(SRV_CONFIG.log_file_name, val_str);
-        SRV_CONFIG.log_file_desc = fopen(val_str, "w");
-
-        if (!SRV_CONFIG.log_file_desc) {
-            fprintf(stderr, "%s(): could not open 'log_file'=%s "
-                    "from %s: %s\n", __func__, val_str,
-                    SRV_CONFIG_FILE, strerror(errno));
-            return -errno;
-        }
+        ret = parse_log_file_name(val_str, true);
+        if (ret)
+            return ret;
     } else if (strcmp(key, "log_level") == 0) {
-        val = atoi(val_str);
-        if (val < LOG_ERROR || val > LOG_INFO2) {
-            fprintf(stderr, "%s(): invalid value %d for 'log_level' "
-                    "in %s, should be in range [%d-%d]\n",
-                    __func__, val, SRV_CONFIG_FILE, LOG_ERROR,
-                    LOG_INFO2);
-            return -ERANGE;
-        }
+        ret = parse_range_value(val_str, &val, LOG_ERROR, LOG_INFO2,
+                                "log_level", true);
+        if (ret)
+            return ret;
+
         SRV_CONFIG.log_level = val;
     }
 
